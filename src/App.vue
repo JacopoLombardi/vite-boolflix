0ee43d2c59e46268ef67a31e1cf3acefaf5329d7
Jacopo Lombardi <38923043+JacopoLombardi@users.noreply.github.com>
2024-04-03 15:23:46 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+
+
+<!-- Javascript -->
+<script>
+
+// import componenti
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+import {store} from './data/store'
+
+export default {
+  components:{
+    Header,
+    Main,
+    Footer
+  },
+
+  data(){
+    return{
+      store
+    };
+  },
+
+  methods:{
+
+  },
+};
+</script>
+
+
+
+<!-- HTML -->
+<template>
+
+  <Header />
+
+  <Main />
+
+  <Footer />
+  
+</template>
+
+
+
+<!-- CSS -->
+<style lang="scss">
+
+@use './assets/scss/main';
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+
+
+<!-- Javascript -->
+<script>
+
+
+export default {
+  data(){
+    return{
+
+    };
+  },
+};
+</script>
+
+
+
+<!-- HTML -->
+<template>
+  
+  
+</template>
+
+
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+@use '../assets/scss/partials/variables' as *;
+
+</style>
